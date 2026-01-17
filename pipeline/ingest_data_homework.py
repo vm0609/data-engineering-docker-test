9bb3bf614b971f679da0c57f1c9ef961201fd155
vm0609 <155051454+vm0609@users.noreply.github.com>
2026-01-17 13:12:35 +0000

    homework files

--- pipeline/ingest_data_homework.py
+++ pipeline/ingest_data_homework.py
@@ -8,6 +8,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 import click
+import numpy as np
 
 dtype = {
     "VendorID": "Int64",
@@ -51,25 +52,65 @@ def run(pg_user,
 
 
 
-    chunksize=100000
+    #chunksize=100000
 
     
     
-
-    file_path = '/workspaces/data-engineering-docker-test/pipeline/data/yellow_tripdata_2025-11.parquet'
+   
+    file_path = '/workspaces/data-engineering-docker-test/pipeline/data/green_tripdata_2025-11.parquet'
     
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
     print('Engine acquired')
     df = pd.read_parquet(file_path)
     print('Read parquet file successfully')
-    print(df.head())
+    
+    print(f'No records in df {len(df)} ')
+    #print(df.head())
        # Load the data into PostgreSQL
-    df.to_sql(name=target_table, con=engine, if_exists='replace', index=False)
+    chunk_size = 500000
+    num_chunks = int(np.ceil(len(df) / chunk_size))
    
+    print(f'nm of chunks is {num_chunks}')
+
+    first = True
+    for i in range(num_chunks):
+        start = i * chunk_size
+        end = (i + 1) * chunk_size
+        df_chunk = df.iloc[start:end]
+
+        if first:
+            # Create the table without data
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists='replace',
+                index=False
+            )
+            print('Table replaced')
+            first = False
+
+        # Append the chunk to the table
+        df_chunk.to_sql(name=target_table, con=engine, if_exists='append', index=False)
+        print(f'Loaded chunk {i + 1} of {num_chunks}')
+
     print(f'Data from {file_path} has been loaded into the {target_table} table.')
  
 
+    # Load taxi_zone_lookup.csv into PostgreSQL
+    csv_file_path = '/workspaces/data-engineering-docker-test/pipeline/data/taxi_zone_lookup.csv'
+    print(f'Loading data from {csv_file_path}')
+    
+    # Read the CSV file
+    df_csv = pd.read_csv(csv_file_path)
+    print('Read CSV file successfully')
+    print(f'No records in CSV: {len(df_csv)}')
+    print(df_csv.head())
+
+    # Load the CSV data into PostgreSQL
+    csv_table_name = 'taxi_zone_lookup'
+    df_csv.to_sql(name=csv_table_name, con=engine, if_exists='replace', index=False)
+    print(f'Data from {csv_file_path} has been loaded into the {csv_table_name} table.')
 
 
 
